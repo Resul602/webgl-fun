778c2330043657644bd25c16b8ed8bd3f2c59cdf
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-06-25 21:59:42 -0400

    cd disc rotation

--- playground.glsl
+++ playground.glsl
@@ -2,19 +2,21 @@ void mainImage(out vec4 fragColour, in vec2 fragCoord) {
     float aspect = iResolution.x/iResolution.y;
     // vec2 uv = (fragCoord * 2. - iResolution.xy) / iResolution.y;
     
-    const float PI = 3.1415; 
 
     vec2 uv = (fragCoord / iResolution.xy) * vec2(2) - vec2(1);
     uv.x *= aspect;
 
     vec2 rotated_uv;
 
+    const float PI = 3.1415;
+    float dist = length(uv);
+
+
     rotated_uv.x = uv.x * sin(iTime/1.);
     rotated_uv.y = uv.y * cos((iTime/1.)+(PI/3.));
 
 
 
-    float dist = length(uv);
 
     //fragColour = vec4(vec3(smoothstep(0., 0.5, abs(uv.y - uv.x))), 0);
     //fragColour = vec4(vec3(abs(uv.y-uv.x)),0);
